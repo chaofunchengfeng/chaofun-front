a6514456a475ed733c6ea53d1578f018adeff17e
panzuhong <332068039@qq.com>
2023-04-13 22:48:59 +0800

    fix(lint): 基础lint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,15 @@ module.exports = {
     node: true
   },
   // extends: ["plugin:vue/essential", "@vue/prettier"],
-  extends: ["plugin:vue/essential"],
+  extends: ['plugin:vue/essential'],
   rules: {
-    "camelcase": 2,
-    "curly": 2,
-    "brace-style": [2, "1tbs"],
-    "quotes": [2, "single"],
-    "semi": [2, "always"],
-    "space-in-brackets": [2, "never"],
-    "space-infix-ops": 2,
+    'curly': 2,
+    'brace-style': [2, '1tbs'],
+    'quotes': [2, 'single'],
+    'semi': [2, 'always'],
+    'space-infix-ops': 2,
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   }
 };
