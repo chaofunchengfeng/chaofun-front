3ef79c4aa6970f8d822e312f9bdfcc4912f2ffbc
panzuhong <332068039@qq.com>
2023-04-19 21:40:12 +0800

    fix: 移除google map标签

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ export default {
   name: 'App',
   mounted() {
     new MutationObserver(() => {
-      const querys = ['a[href^="https://maps.google.com/maps"]', 'a[href^="http://maps.google.com/maps"]', 'a[href^="http://www.google.com/maps"]', '.gm-iv-address', '.gm-style-cc'];
+      const querys = ['a[href^="https://maps.google.com/maps"]', 'a[href^="http://maps.google.com/maps"]', 'a[href^="https://www.google.com/maps"]', 'a[href^="http://www.google.com/maps"]', '.gm-iv-address', '.gm-style-cc'];
       querys.forEach(query => {
         const all = document.querySelectorAll(query);
         all.forEach(item => {
