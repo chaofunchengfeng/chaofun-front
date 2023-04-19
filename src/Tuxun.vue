1782896490f36574b3d660d146403f14339480eb
panzuhong <332068039@qq.com>
2023-04-19 21:58:23 +0800

    fix: 增加tuxun监听删除的代码

--- src/Tuxun.vue
+++ src/Tuxun.vue
@@ -6,6 +6,17 @@
 
 <script>
 export default {
-  name: 'tuxun-index'
+  name: 'tuxun-index',
+  mounted() {
+    new MutationObserver(() => {
+      const querys = ['a[href^="https://maps.google.com/maps"]', 'a[href^="http://maps.google.com/maps"]', 'a[href^="https://www.google.com/maps"]', 'a[href^="http://www.google.com/maps"]', '.gm-iv-address', '.gm-style-cc'];
+      querys.forEach(query => {
+        const all = document.querySelectorAll(query);
+        all.forEach(item => {
+          item.remove();
+        });
+      });
+    }).observe(document.getElementById('app'), { attributes: false, childList: true, subtree: true });
+  }
 };
 </script>
