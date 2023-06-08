39f59ceb4e3ee6f09e54ae7589165f8cfb7acd39
cijianzy <cijianzy@gmail.com>
2023-06-08 23:31:05 +0800

    fix: 搜索用户

--- src/views/tuxun/friend.vue
+++ src/views/tuxun/friend.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <div class="top-left">
       <el-button @click="goHome" round>首页</el-button>
+      <el-button @click="openSearch" round>搜索添加</el-button>
     </div>
     <div style=" margin: auto; margin-top: 4rem; width: 640px;max-width: 100%;padding: 10px;">
       <div v-for="(item,index) in friends" :key="index" class="list" @click="toUser(item)">
@@ -12,6 +13,27 @@
       </div>
       <div class="footer">
       </div>
+    </div>
+
+    <div v-if="searchOpen" class="search">
+        <section class="section" style="display: block;overflow:scroll; overscroll-behavior: contain; height: 100%;    scrollbar-width: none;">
+          <div style="font-size: 20px; color: white; padding-top: 2rem">用户搜索<el-button type="primary" style="margin-left: 10px" @click="closeSearch" round>关闭</el-button></div>
+          <el-input v-model="keyword"
+                    :autofocus="true"
+                    @input="search"
+                    placeholder="搜索用户"
+                    style="margin-top: 2rem; max-width: 100%; margin-bottom: 20px"></el-input>
+          <div v-for="(item, index) in this.users" class="user" >
+            <div @click.stop="openUser(item)" style="display: flex;  justify-content: space-between;">
+              <div style="height: 100%;display: flex; color: white">
+                <div style="padding-bottom: 5px">
+                  {{item.userName}} (uid: {{item.userId}})
+                </div>
+              </div>
+            </div>
+            <div style="margin-top: 1px; width: 100%;height: 1px; background-color: white;margin-bottom: 8px"></div>
+          </div>
+        </section>
     </div>
   </div>
 </template>
@@ -27,6 +49,9 @@ export default {
   data() {
     return {
       friends: [],
+      searchOpen: false,
+      users: [],
+      keyword: '',
       timer: 0
     };
   },
@@ -34,11 +59,6 @@ export default {
   },
   mounted() {
     this.getFriends();
-
-    let _this = this;
-    setInterval(() => {
-      _this.getButtonRank();
-    }, 5000);
   },
   methods: {
     getFriends() {
@@ -49,14 +69,73 @@ export default {
     toUser(item) {
       tuxunJump('/tuxun/user/' + item.userId);
     },
+    openUser(item) {
+      tuxunOpen('/tuxun/user/' + item.userId);
+    },
 
     goHome() {
       tuxunJump('/tuxun/');
     },
+    openSearch() {
+      this.searchOpen = true;
+      this.search('');
+    },
+    closeSearch() {
+      this.searchOpen = false;
+    },
+    search(keyword) {
+      api.getSearchUser({'keyword': keyword, 'pageNum': 1}).then((res) => {
+        this.users = res.data.data;
+
+      });
+    }
   }
 };
 </script>
 <style lang="scss" scoped>
+::v-deep .el-input__inner {
+  background-color: rgba(255, 255, 255, 0.247);
+  color: white;
+}
+.search {
+  position: fixed;
+  z-index: 2012;
+  height: 100%;
+  width: 100%;
+  //color: ;
+  top: 0;
+  background-color: #090723;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+
+  .back_home {
+    position: fixed;
+    padding-top: 1rem;
+    padding-left: 1rem;
+    z-index: 500;
+  }
+
+  .section{
+    width: 60%;
+    -ms-overflow-style: none;  /* Internet Explorer 10+ */
+    scrollbar-width: none;  /* Firefox */
+  }
+  .section::-webkit-scrollbar {
+    display: none;  /* Safari and Chrome */
+  }
+
+  .user {
+    cursor: pointer;
+    &:hover {
+      background-color: grey;
+    }
+  }
+
+}
 .container {
   width: 100%;
   min-height: 100%;
@@ -66,6 +145,7 @@ export default {
     padding-top: 2rem;
     padding-left: 2rem;
   }
+
 }
 div {
   color: white;
@@ -99,6 +179,11 @@ div {
     .top-left {
       padding-left: 1rem;
       padding-top: 1rem;
+    }
+  }
+  .cover {
+    .section {
+      width: 90%;
     }
   }
 }
---
+++ src/views/tuxun/user-search.vue
@@ -0,0 +1,157 @@
+<template>
+  <div v-if="show" class="cover">
+    <section class="section" style="display: block;overflow:scroll; overscroll-behavior: contain; height: 100%;    scrollbar-width: none;">
+      <div style="font-size: 20px; color: white; padding-top: 2rem">练习题库搜索<el-button type="primary" style="margin-left: 10px" @click="close" round>关闭</el-button></div>
+      <el-input v-model="keyword"
+                :autofocus="true"
+                @input="search"
+                placeholder="搜索练习题库"
+                style="margin-top: 2rem; max-width: 100%; margin-bottom: 20px"></el-input>
+      <div v-for="(item, index) in this.pagedata" >
+        <div @click.stop="toMapsDetail(item)" style="display: flex;  justify-content: space-between;">
+          <div style="height: 100%;display: flex; color: white">
+            <div>
+              {{item.name}}
+              <div>
+                <span style="font-size: 6px;bottom: 0px; color: grey">(<span v-if="item.pcount">地点: {{item.pcount}} | </span>人次: {{item.players}} <span v-if="item.canMove"> | 可移动 </span>)</span>
+              </div>
+            </div>
+          </div>
+          <div v-if="callBack">
+            <el-button style="background-color: unset; color: white" @click.stop="toMaps(item, 'noMove')" type="primary" round>固定</el-button>
+            <el-button style="background-color: unset; color: white" @click.stop="toMaps(item, 'move')" type="primary" v-if="item.canMove" round>移动<span style="color: gold">(VIP)</span></el-button>
+            <div v-if="!item.canMove" style="width: 60px"></div>
+          </div>
+          <div v-else>
+            <el-button style="background-color: unset; color: white" @click.stop="toMapsNew(item)" type="primary" round>探索<span  style="color: gold">(VIP)</span></el-button>
+          </div>
+        </div>
+        <div style="margin-top: 1px; width: 100%;height: 1px; background-color: white;margin-bottom: 8px"></div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import * as api from '@/api/api';
+import {tuxunJump} from '../../../../views/tuxun/common';
+export default {
+  name: 'mapsSearch',
+  data() {
+    return {
+      show: false,
+      keyword: '',
+      overflow: null,
+      pagedata: null
+    };
+  },
+  mounted() {
+    this.search();
+    this.overflow = document.body.style.overflow;
+    document.body.style.overflow = 'hidden';
+  },
+  methods: {
+    search() {
+      api.getByPath('/api/v0/tuxun/maps/search', {keyword: this.keyword}).then(res=>{
+        this.pagedata = res.data;
+      });
+    },
+    toMapsDetail(item) {
+      // console.log(this.callBack)
+      if (!this.callBack) {
+        tuxunJump('/tuxun/maps_detail?mapsId=' + item.id);
+      }
+    },
+    toMaps(item, type) {
+      if (!this.callBack) {
+        api.getByPath('/api/v0/tuxun/game/enterMap', {mapsId: item.id}).then(res => {
+        });
+
+        api.getByPath('/api/v0/tuxun/challenge/create', {'mapsId': item.id, 'type': type}).then(res => {
+          if (res.success) {
+            tuxunJump('/tuxun/challenge?challengeId=' + res.data);
+          } else {
+            if (res.errorCode === 'need_vip') {
+              this.$vip({});
+            }
+          }
+        });
+      } else {
+        this.close();
+        this.callBack(item.id, type);
+      }
+    },
+    toMapsNew(item){
+      api.getByPath('/api/v0/tuxun/vip/check').then(res=>{
+        if (res.success) {
+          if (res.data) {
+            tuxunJump('/tuxun/maps-start?mapsId=' + item.id);
+          } else {
+            this.$vip();
+          }
+        } else {
+          if (res.errorCode === 'need_login') {
+            this.$login({
+              callBack: () => {
+                this.$store.dispatch('user/getInfo');
+              },
+            });
+          }
+        }
+      });
+    },
+    close() {
+      this.show = false;
+      document.body.style.overflow = this.overflow;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+::v-deep .el-input__inner {
+  background-color: rgba(255, 255, 255, 0.247);
+  color: white;
+}
+.cover {
+  position: fixed;
+  z-index: 2012;
+  height: 100%;
+  width: 100%;
+  //color: ;
+  top: 0;
+  background-color: #090723;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+
+  .back_home {
+    position: fixed;
+    padding-top: 1rem;
+    padding-left: 1rem;
+    z-index: 500;
+  }
+
+  .section{
+    width: 60%;
+    -ms-overflow-style: none;  /* Internet Explorer 10+ */
+    scrollbar-width: none;  /* Firefox */
+  }
+  .section::-webkit-scrollbar {
+    display: none;  /* Safari and Chrome */
+  }
+
+
+}
+
+@media only screen and (max-width: 768px) {
+  .cover{
+    .section {
+      width: 90%;
+    }
+  }
+}
+</style>
