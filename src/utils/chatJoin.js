c21065852f79681619a050c94f98d68ba7e8a783
chaofunchengfeng <chaofunchengfeng@qq.com>
2023-08-15 18:10:12 +0800

    fix:safari无法打开新标签页的问题 https://choa.fun/p/1275082

--- src/utils/chatJoin.js
+++ src/utils/chatJoin.js
@@ -1,5 +1,5 @@
-import { startSingleChat } from '@/api/api';
-import Vue from 'vue';
+import { startSingleChat } from "@/api/api";
+import Vue from "vue";
 
 export function startSingle(userId) {
   startSingleChat({
@@ -7,8 +7,13 @@ export function startSingle(userId) {
   }).then(res => {
     if (res) {
       if (res.data) {
-        localStorage.setItem('chao.fun.chat.newSingleChatChannel', JSON.stringify(res.data));
-        window.open('/chat?id=' + res.data.id, '_blank');
+        localStorage.setItem("chao.fun.chat.newSingleChatChannel", JSON.stringify(res.data));
+
+        // fix:safari无法打开新标签页的问题
+        setTimeout(() => {
+          window.open("/chat?id=" + res.data.id, "_blank");
+        }, 20);
+
       } else {
         Vue.prototype.$toast(res.errorMessage);
       }
