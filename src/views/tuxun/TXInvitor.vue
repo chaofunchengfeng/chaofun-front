66f3d1fea921133ab51cd91c37ce1b5101293cff
cijianzy <cijianzy@gmail.com>
2023-09-16 20:12:07 +0800

    fix: 支持上报 gameId

--- src/views/tuxun/TXInvitor.vue
+++ src/views/tuxun/TXInvitor.vue
@@ -910,7 +910,7 @@ export default {
                     this.viewer.addListener('status_changed', () => {
                       console.log(this.viewer.getStatus());
                       if (this.viewer.getStatus() && this.viewer.getStatus() !== 'OK') {
-                        api.getByPath("/api/v0/tuxun/client/report", {panoId: this.viewer.getPano(), status: this.viewer.getStatus(), page: 'tuxun_invitor'}).then(res => {
+                        api.getByPath("/api/v0/tuxun/client/report", {panoId: this.viewer.getPano(), status: this.viewer.getStatus(), page: 'tuxun_invitor', gameId: this.gameId}).then(res => {
                         });
                       }
                     });
