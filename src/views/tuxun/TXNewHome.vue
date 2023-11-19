d7fb28df25bd7d1b803bd9198e9d89050061efc8
cijianzy <cijianzy@gmail.com>
2023-11-19 15:48:23 +0800

    fix: 去掉点击事件

--- src/views/tuxun/TXNewHome.vue
+++ src/views/tuxun/TXNewHome.vue
@@ -32,7 +32,7 @@
       <div style="height: 20px"></div>
 
       <div class="info" @click="toFirstTournament">图寻高校争霸赛圆满收官~</div>
-      <div class="second-info" @click="toSecond"> 如打不开页面或者一直加载中，请尝试清理浏览器缓存或者重装App </div>
+      <div class="second-info"> 如打不开页面或者一直加载中，请尝试清理浏览器缓存或者重装App </div>
 
       <section class="game_entrance">
         <div class="first_session_head">单人</div>
