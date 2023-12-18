8a3bbefdc2430ede90eefd1b825558ea80f214c9
cijianzy <cijianzy@gmail.com>
2023-12-18 19:51:04 +0800

    fix: 支持揭图

--- src/views/tuxun/TXNewHome.vue
+++ src/views/tuxun/TXNewHome.vue
@@ -67,6 +67,14 @@
             </div>
             <div class="card-top-right">可移动</div>
           </div>
+          <div class="card" @click="redirectPage( 'https://tuxun.fun/uncover')">
+            <div class="title">
+              揭图
+            </div>
+            <div class="describe">
+              图片题，新模式
+            </div>
+          </div>
         </div>
 
         <div class="first_session_head">积分</div>
